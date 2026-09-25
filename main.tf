7e1c3087f32c171ae25e757e2b6e538e79a9440f
benkorichard <benko.richard86@gmail.com>
2026-09-25 16:57:55 +0200

    Fix deprecated attribute

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "aws_route_table_association" "private" {
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.${data.aws_region.current.id}.s3"
+  service_name      = "com.amazonaws.${data.aws_region.current.region}.s3"
   vpc_endpoint_type = "Gateway"
   route_table_ids = concat(
     [aws_route_table.public.id],
